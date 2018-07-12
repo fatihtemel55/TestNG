6e49a81e2e3c0409eaa5b82b713ea4c0f3081a2f
Fatih T. <fatihtemel55@gmail.com>
2018-07-11 19:54:09 -0500

    seseses

---
+++ src/arraylist/Main.java
@@ -0,0 +1,19 @@
+package arraylist;
+
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Bag<Integer> bag =  new Bag<>();
+		ArrayList<Integer> a = new ArrayList<>();
+		for(int i = 1; i <= 1000000; i++) {
+			bag.add(i*10);
+			a.add(i);
+		}
+		System.out.println("Bag size:" + bag.size());
+
+
+	}
+
+}
